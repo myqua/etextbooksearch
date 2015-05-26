fed82d6a53f6fee47b91c57634f1af0a5dd03460
Mike Waugh <mwaugh2@lsu.edu>
2015-05-26 15:27:48 -0500

    adds xisbn webservice, parsing with element.tree

--- findISBNinText.py
+++ findISBNinText.py
@@ -1,5 +1,8 @@
 import re
 import string
+import requests
+from xml.etree import ElementTree
+
 isbnPattern1 = re.compile(r'978(?:-?\d){10}')
 isbnPattern2 = re.compile(r'[A-Za-z]((?:-?\d){10})\D')
 isbnPattern3 = re.compile(r'[A-zA-Z]((?:-?\d){9}X)')
@@ -12,10 +15,22 @@ with open("SummerTextbooks2015.txt") as isbn_lines:
         isbns.extend(isbnPattern2.findall(line))
         isbns.extend(isbnPattern3.findall(line))
         isbns.extend(isbnPattern4.findall(line))
-stripped = []
-for y in isbns:
-    stripped.append(y.translate(str.maketrans('','','-')))
-for x in range(10):
-    print (stripped[x])
+stripped = '9781118162286'
+#for y in isbns:
+ #   stripped.append(y.translate(str.maketrans('','','-')))
+#for z in stripped:
+urlstripped = 'http://xisbn.worldcat.org/webservices/xid/isbn/'+stripped
+response = requests.get(urlstripped)
+tree = ElementTree.fromstring(response.content)
+for child in tree:
+    print (child.text)
 
+#match the files
+#with open('AllPublisherISBNs.txt','r') as f:
+#    isbn = [line.strip() for line in f]
+#matches = []
+#for x in isbn:
+#    if x in stripped:
+#        matches.append(x)
+#print (matches)
 
