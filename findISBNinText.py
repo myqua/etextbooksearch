4e082f102970a65e323e06cecda915ee2fa84c6a
Mike Waugh <mwaugh2@lsu.edu>
2015-05-29 12:01:33 -0500

    prints book metadata and includes catalog file

--- findISBNinText.py
+++ findISBNinText.py
@@ -1,6 +1,7 @@
 import re
 import string
 import requests
+import csv
 from xml.etree import ElementTree
 
 isbnPattern1 = re.compile(r'978(?:-?\d){10}')
@@ -9,7 +10,7 @@ isbnPattern3 = re.compile(r'[A-zA-Z]((?:-?\d){9}X)')
 isbnPattern4 = re.compile(r'a(\d{10})\D')
 isbns = []
 #with open("CRCRoughISBN.txt", encoding="utf8") as isbn_lines: #use for utf8
-with open("UpdatedListSpring2015.txt") as isbn_lines:
+with open("SummerTextbooks2015.txt") as isbn_lines:
     for line in isbn_lines:
         isbns.extend(isbnPattern1.findall(line))
         isbns.extend(isbnPattern2.findall(line))
@@ -20,10 +21,6 @@ for y in isbns:
     stripped.append(y.translate(str.maketrans('','','-')))
     stripped = list(set(stripped))
 expandedisbns = []
-#with open ("Summer2015LSU-isbn.txt") as alreadyKnown:
-#    ak = [thing.strip() for thing in alreadyKnown]
-#newOnes = [u for u in stripped if u not in ak]
-#print (newOnes)
 # only run this part when you need to query webservice
 for z in stripped:
     urlstripped = 'http://xisbn.worldcat.org/webservices/xid/isbn/'+z+'?method=getEditions&format=xml&ai=mike.waugh'
@@ -31,20 +28,40 @@ for z in stripped:
     tree = ElementTree.fromstring(response.content)
     for child in tree:
            expandedisbns.append(child.text)
+           
 #need to print these out to get around xisbn throttling of webservice requests
-with open("expandedISBNSpring.txt", "w") as outfile:
+with open("expandedISBNSummer2.txt", "w") as outfile:
     for item in expandedisbns:
         outfile.write("%s\n" % item)
-with open("expandedISBNSpring.txt", "r") as i:
+with open("expandedISBNSummer2.txt", "r") as i:
     coursebooks = [book.strip() for book in i]
 #match the files
 with open('AllPublisherISBNs.txt','r') as f:
     isbn = [line.strip() for line in f]
+with open ("EbooksByItemCat201412.txt", "r") as catfile:
+    catlines = [r.strip() for r in catfile]
+isbn = list(set(isbn + catlines))
 matches = []
 for x in isbn:
     if x in coursebooks:
         matches.append(x)
-with open("MatchedISBNsSpring.txt", "w") as outfile:
+with open("MatchedISBNsSummer2.txt", "w") as outfile:
    for item in matches:
         outfile.write("%s\n" % item)
+        
+# get metadata from xisbn
+rows = []
+for z in matches:
+    urlz = 'http://xisbn.worldcat.org/webservices/xid/isbn/'+z+'?method=getMetadata&format=xml&fl=*&ai=mike.waugh'
+    response = requests.get(urlz)
+    tree = ElementTree.fromstring(response.content)
+    for child in tree:
+        rows.append(child.attrib)
 
+# print to csv
+with open ("MatchedEtextbooksSummer2015-2.csv", "w") as csvfile:
+    fieldnames = rows[1].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, lineterminator='\n', extrasaction='ignore')
+    writer.writeheader()
+    for row in rows:
+        writer.writerow(row)
