751a2adaf7e7ee478505c392557c57538b72c8ef
Mike Waugh <mwaugh2@lsu.edu>
2015-05-29 10:17:36 -0500

    outputs to file

--- findISBNinText.py
+++ findISBNinText.py
@@ -9,29 +9,33 @@ isbnPattern3 = re.compile(r'[A-zA-Z]((?:-?\d){9}X)')
 isbnPattern4 = re.compile(r'a(\d{10})\D')
 isbns = []
 #with open("CRCRoughISBN.txt", encoding="utf8") as isbn_lines: #use for utf8
-#with open("SummerTextbooks2015.txt") as isbn_lines:
-#    for line in isbn_lines:
-#        isbns.extend(isbnPattern1.findall(line))
-#        isbns.extend(isbnPattern2.findall(line))
-#        isbns.extend(isbnPattern3.findall(line))
-#       isbns.extend(isbnPattern4.findall(line))
-#stripped = []
-#for y in isbns:
-#    stripped.append(y.translate(str.maketrans('','','-')))
-#    stripped = list(set(stripped))
-#expandedisbns = []
+with open("UpdatedListSpring2015.txt") as isbn_lines:
+    for line in isbn_lines:
+        isbns.extend(isbnPattern1.findall(line))
+        isbns.extend(isbnPattern2.findall(line))
+        isbns.extend(isbnPattern3.findall(line))
+        isbns.extend(isbnPattern4.findall(line))
+stripped = []
+for y in isbns:
+    stripped.append(y.translate(str.maketrans('','','-')))
+    stripped = list(set(stripped))
+expandedisbns = []
+#with open ("Summer2015LSU-isbn.txt") as alreadyKnown:
+#    ak = [thing.strip() for thing in alreadyKnown]
+#newOnes = [u for u in stripped if u not in ak]
+#print (newOnes)
 # only run this part when you need to query webservice
-#for z in stripped:
-#    urlstripped = 'http://xisbn.worldcat.org/webservices/xid/isbn/'+z+'?method=getEditions&format=xml&ai=mike.waugh'
-#    response = requests.get(urlstripped)
-#    tree = ElementTree.fromstring(response.content)
-#    for child in tree:
-#           expandedisbns.append(child.text)
+for z in stripped:
+    urlstripped = 'http://xisbn.worldcat.org/webservices/xid/isbn/'+z+'?method=getEditions&format=xml&ai=mike.waugh'
+    response = requests.get(urlstripped)
+    tree = ElementTree.fromstring(response.content)
+    for child in tree:
+           expandedisbns.append(child.text)
 #need to print these out to get around xisbn throttling of webservice requests
-#with open("expandedISBNs.txt", "w") as outfile:
-#    for item in expandedisbns:
- #       outfile.write("%s\n" % item)
-with open("expandedISBNs.txt", "r") as i:
+with open("expandedISBNSpring.txt", "w") as outfile:
+    for item in expandedisbns:
+        outfile.write("%s\n" % item)
+with open("expandedISBNSpring.txt", "r") as i:
     coursebooks = [book.strip() for book in i]
 #match the files
 with open('AllPublisherISBNs.txt','r') as f:
@@ -40,7 +44,7 @@ matches = []
 for x in isbn:
     if x in coursebooks:
         matches.append(x)
-with open("MatchedISBNs.txt", "w") as outfile:
-    for item in matches:
+with open("MatchedISBNsSpring.txt", "w") as outfile:
+   for item in matches:
         outfile.write("%s\n" % item)
 
